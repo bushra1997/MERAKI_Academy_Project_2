4c0dd20ba01b22a68c344c453ecb61c7a8491e8a
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-05-03 13:57:26 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
-$(document).ready(()=>{
-    $(window).scroll(() => {
-        $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
-    });
-      
-    $("#btn-more").on("click", () => {
-    $(".more").fadeIn(),
-    $("#btn-more").fadeOut();
-    });
-      
-    $(window).scroll(() => {
-    if ($(this).scrollTop()>780){
-        $('#up-button').fadeIn()
+$(document).ready(() => {
+  $(window).scroll(() => {
+    $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
+  });
+
+  $("#btn-more").on("click", () => {
+    $(".more").fadeIn(), $("#btn-more").fadeOut();
+  });
+
+  $('#btn-more-articles').on('click', () => {
+    $('#btn-more-articles').fadeOut();
+  })
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 780) {
+      $("#up-button").fadeIn();
+    } else {
+      $("#up-button").fadeOut();
+    }
+  });
+  $("#up-button").on("click", () => {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
+
+//   for search bar
+  let array = [
+    "Cheesecake",
+    "Strawberry",
+    "Vanilla",
+    "Cake",
+    "Chocolate",
+    "Raspberry",
+    "Lemonade",
+    "Smoothie",
+    "Blueberry",
+    "Muffin",
+  ];
+  $('#search').on('click',() => {
+    array.forEach((element, index) =>{
+        if ($('.form-control').val().toLowerCase() === element.toLowerCase()){
+    
         }else{
-        $('#up-button').fadeOut()   
+    
         }
-    });
-    $('#up-button').on('click', () => {
-        $("html, body").animate({
-            scrollTop: 0,
-        }, 1500);
-    })
-})
+      })
+  })
+});
